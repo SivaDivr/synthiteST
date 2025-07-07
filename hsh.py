4166e5a3d050fe6cbc6083ca00bb45b307a2195c
siva7077 <sivacdl77@gmail.com>
2025-07-07 17:35:11 +0530

    Update: added streamlit app.py and cleaned up code2

--- hsh.py
+++ hsh.py
@@ -2,7 +2,7 @@ import streamlit_authenticator as stauth
 
 # Correct way to use Hasher in recent versions
 hasher = stauth.Hasher()
-hashed_passwords = hasher.hash('1234')
+hashed_passwords = hasher.hash('P@ssw0rd')
 
 print(hashed_passwords)
 
