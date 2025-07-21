727881f25e50344300fc8bcc195be86358396ff9
SivaDivr <157581435+SivaDivr@users.noreply.github.com>
2025-07-21 18:26:20 +0530

    Update appst.py

--- appst.py
+++ appst.py
@@ -25,17 +25,17 @@ st.set_page_config(page_title="Synthite AI", layout="wide")
 
 
 # Hide all Streamlit default UI elements (header, menu, footer, profile icons)
-# hide_streamlit_style = """
-#     <style>
-#     #MainMenu {visibility: hidden;}
-#     footer {visibility: hidden;}
-#     header {visibility: hidden;}
-#     .stDeployButton {display:none;}
-#     .viewerBadge_container__1QSob {display: none;}
-#     </style>
-# """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-# st.set_page_config(page_title="Synthite AI", layout="centered")
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    .stDeployButton {display:none;}
+    .viewerBadge_container__1QSob {display: none;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.set_page_config(page_title="Synthite AI", layout="centered")
 
 # # Hide Streamlit branding, footer, menu, and profile icon
 # hide_streamlit_style = """
